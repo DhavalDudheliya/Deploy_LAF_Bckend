0247d40e796f4748518a79b908988a8de1d0eda9
DhavalDudheliya <dhavaldudheliya77@gmail.com>
2023-04-28 14:03:21 +0530

    Second Commit

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.use(
   })
 );
 const corsOptions = {
-  origin: "http://localhost:3000",
+  origin: "*",
   credentials: true, //access-control-allow-credentials:true
   optionSuccessStatus: 200,
 };
